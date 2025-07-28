7a5d72c4026168f804fd2c48a266e42adff84b90
cyber <ojeikelvin@gmail.com>
2025-07-28 04:34:46 +0100

    just learn how to use ternary operator to determin how to assign value image when true or false

--- complex-state-object/COMPLEX-STATE-OBJECT/src/App.jsx
+++ complex-state-object/COMPLEX-STATE-OBJECT/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import starEmpty from "./assets/IMAGES/download.jpg";
+import filled from "./assets/IMAGES/filled.png";
+import empty from "./assets/IMAGES/empty.png";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (212) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: true,
+  });
+
+  let starIcon = contact.isFavorite ? filled : empty;
+
+  function toggleFavorite() {
+    console.log("Toggle Favorite");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <article className="card">
+        <img
+          src={starEmpty}
+          className="avatar"
+          alt="User profile picture of John Doe"
+        />
+        <div className="info">
+          <button
+            onClick={toggleFavorite}
+            aria-pressed={contact.isFavorite}
+            aria-label={
+              contact.isFavorite ? "Remove from favorites" : "Add to favorites"
+            }
+            className="favorite-button"
+          >
+            <img
+              src={starIcon}
+              alt={contact.isFavorite ? "filled star icon" : "empty start icon"}
+              className="favorite"
+            />
+          </button>
+          <h2 className="name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="contact">{contact.phone}</p>
+          <p className="contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  );
 }
-
-export default App
